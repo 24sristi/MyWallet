8051149091f03d350c003ca3bf7e114c66bc260a
24sristi <24sristi@gmail.com>
2020-10-25 01:21:05 +0530

    new commit

---
+++ app/src/main/java/com/example/mywallet/Debit.java
@@ -0,0 +1,30 @@
+package com.example.mywallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Debit extends AppCompatActivity {
+
+    EditText debitamount;
+    EditText debitreason;
+    DatabaseHandler dbHandler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debit);
+        debitamount= (EditText) findViewById(R.id.debitamount);
+        debitreason= (EditText) findViewById(R.id.debitreason);
+        dbHandler = new DatabaseHandler(this, null, null, 2);
+        Button submit = (Button) findViewById(R.id.submitdebit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                dbHandler.addcredit(debitamount.getText().toString(),debitreason.getText().toString());
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/mywallet/MainActivity.java
+++ app/src/main/java/com/example/mywallet/MainActivity.java
@@ -14,10 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button credit = (Button) findViewById(R.id.credit);
+        Button debit = (Button) findViewById(R.id.debit);
         credit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
                 Intent intent = new Intent(getApplicationContext(), Credit.class);
+                startActivity(intent);
+            }
+        });
+        debit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Intent intent = new Intent(getApplicationContext(), Debit.class);
                 startActivity(intent);
             }
         });
